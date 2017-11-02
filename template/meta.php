3666cd77a66a1f83fa04a6c684755d0b791983e5
J0cker <manlioelnum1@hotmail.com>
2017-11-01 18:17:49 -0600

    google tag meta

--- template/meta.php
+++ template/meta.php
@@ -136,3 +136,13 @@ $( document ).ready(function() {
     getLastPostBlog();
 });
 </script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109145959-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-109145959-1');
+</script>
