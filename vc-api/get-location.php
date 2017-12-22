862025e6e5b2edf12087444e2f8de20bb1fe12ab
J0cker <manlioelnum1@hotmail.com>
2017-12-22 11:28:07 -0600

    API VC

--- vc-api/get-location.php
+++ vc-api/get-location.php
@@ -1,5 +1,45 @@
 <?PHP
 
+header("Content-Type: text/html;charset=utf-8");
 
+include ''.dirname(__FILE__).'/../scripts/utils/conexion.php';
+include ''.dirname(__FILE__).'/../scripts/utils/functions.php';
+include ''.dirname(__FILE__).'/../scripts/utils/log.php';
+
+mysqli_set_charset($conn,"utf8");
+
+$log = new LoggerPhp();
+$log->write_log("[Get-location]","Debug");
+
+$obj = new stdclass();
+$obj->success = "true";
+$obj->description = "";
+$obj->error = 0;
+
+$obj->locations = array();
+
+$query = $conn->query("SELECT * FROM locationsapi") OR die(mysqli_error($conn));
+if($query->num_rows>0){
+
+    $c=0;
+
+    while($row=$query->fetch_assoc()){
+
+        $obj->locations[] = new stdclass();
+     
+        $obj->locations[$c]->id = $row["id"];
+        $obj->locations[$c]->name = $row["name"];
+
+        $c++;
+    }
+
+} else {
+    
+    $obj->success = "false";
+    $obj->description = "Error Contacte al administrador";
+    $obj->error = 1;
+}
+
+echo json_encode($obj);
 
 ?>
---
+++ vc-api/get-voucher.php
@@ -0,0 +1,97 @@
+<?PHP
+
+header("Content-Type: text/html;charset=utf-8");
+
+include ''.dirname(__FILE__).'/../scripts/utils/conexion.php';
+include ''.dirname(__FILE__).'/../scripts/utils/functions.php';
+include ''.dirname(__FILE__).'/../scripts/utils/log.php';
+
+mysqli_set_charset($conn,"utf8");
+
+$log = new LoggerPhp();
+$log->write_log("[Get-location]","Debug");
+
+$option = $_GET["option"];
+$location = $_GET["location"];
+$macAddress = $_GET["macAddress"];
+
+
+$obj = new stdclass();
+$obj->success = "true";
+$obj->description = "";
+$obj->voucher = "";
+$obj->error = 0;
+
+//Busca MacAddress Repetidas
+//Método de Exchange between Piggi Download and Free Internet
+if($option==1){
+
+    //busca MACAddress
+    $query = $conn->query("SELECT * FROM macaddressapi WHERE address='".$macAddress."'") OR die(mysqli_error($conn));
+    if($query->num_rows>0){
+        
+        //rechaza la entrega de voucher
+        $obj->success = "false";
+        $obj->description = "Éste usuario ya ha descargado previamente la aplicación piggi.";
+        $obj->error = 2;
+            
+
+    } else {
+
+        //busca un Voucher
+        $query = $conn->query("SELECT * FROM vouchersapi WHERE location_id='".$location."' ORDER BY id ASC") OR die(mysqli_error($conn));
+        if($query->num_rows>0){
+
+            $row=$query->fetch_assoc();
+
+            $obj->voucher = $row["voucher"];
+
+            //almacena MacAddress en Base
+
+            $insert = $conn->query("INSERT INTO macaddressapi (address) VALUES ('".$macAddress."')") OR die(mysqli_error($conn));
+            if($insert!=1){
+
+                $obj->success = "false";
+                $obj->description = "Error Contacte al administrador";
+                $obj->error = 1;
+
+            }
+
+            //falta eliminar voucher
+
+        } else {
+            
+            //no hay vouchers disponibles
+            $obj->success = "false";
+            $obj->description = "No hay vouchers disponibles";
+            $obj->error = 3;
+        }
+
+    }
+
+} else {
+
+    //NO Busca MacAddress Repetidas
+    //Método de Exchange between piggi coins and Free Internet
+    //busca un VOucher
+    $query = $conn->query("SELECT * FROM vouchersapi WHERE location_id='".$location."' ORDER BY id ASC") OR die(mysqli_error($conn));
+    if($query->num_rows>0){
+
+        $row=$query->fetch_assoc();
+
+        $obj->voucher = $row["voucher"];
+
+        //falta eliminar voucher
+
+    } else {
+        
+        //no hay vouchers disponibles
+        $obj->success = "false";
+        $obj->description = "No hay vouchers disponibles";
+        $obj->error = 3;
+    }
+}
+
+echo json_encode($obj);
+
+?>
